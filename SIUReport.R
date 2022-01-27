6d501edc7b54f4384ace796ec0bbaf2a834f51bb
Adam <adam@adamoxford.co.uk>
2022-01-27 12:14:00 +0200

    Updated after further cleaning 2

--- SIUReport.R
+++ SIUReport.R
@@ -5,8 +5,8 @@ library(tidyverse)
 SIUReport <- read.csv("SIUReferrals.csv")
 
 SIUReport <- SIUReport %>% 
-  rename(State_Institution = State.Instituion,
-         Name_or_Organisation = Name.Organisation,
+  rename(State_Institution = State.Institution,
+         Name_or_Organisation = Name.and.Job.Title,
          No_of_referrals = No.of.referrals, 
          Date_referred = Date.referred,
          Progress_to_date = Progress.to.date)
