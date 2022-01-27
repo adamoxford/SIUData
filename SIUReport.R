73832beb77a714ee38d33f7ff6ca1faa2423d703
Adam <adam@adamoxford.co.uk>
2022-01-27 19:34:57 +0200

    new index

--- SIUReport.R
+++ SIUReport.R
@@ -6,11 +6,11 @@ SIUReport <- read.csv("SIUReferrals.csv")
 
 SIUReport <- SIUReport %>% 
   rename(State_Institution = State.Institution,
-         Name_or_Organisation = Name.and.Job.Title,
+         Name_or_Organisation = Name.or.Organisation,
          No_of_referrals = No.of.referrals, 
          Date_referred = Date.referred,
          Progress_to_date = Progress.to.date)
 
-t <- datatable(SIUReport, filter = "top", height = "1000", fillContainer = TRUE)
+t <- datatable(SIUReport, filter = "top", fillContainer = TRUE)
 
 t
